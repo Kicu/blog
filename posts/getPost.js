d6cb453a67e91684f1581644e1046a984b59cf98
Mateusz Titz <titzmateusz@gmail.com>
2021-01-05 15:03:43 +0100

    chore: test commit credentials

--- posts/getPost.js
+++ posts/getPost.js
@@ -6,6 +6,7 @@ import markdownToHtml from './markdownToHtml';
 const readFile = promisify(fs.readFile);
 
 function getPost(slug) {
+  
   const filePath = `posts/db/${slug}.md`;
 
   return readFile(filePath, 'utf8')
