f45636cebd4cc57ace5608c861b9a346b4d20de4
Mateusz Titz <titzmateusz@gmail.com>
2021-03-29 19:11:27 +0200

    fix: fix bug in generating post metadata

--- src/posts/extractPostMetadata.ts
+++ src/posts/extractPostMetadata.ts
@@ -3,7 +3,7 @@ function extractPostMetadata(data: any): PostMetadata {
   const id = 'id' in data ? data.id : '';
   const slug = 'slug' in data ? data.slug : '';
   const createdDate = 'createdDate' in data ? data.createdDate : '2020-02-20'; // Todo add sensible default
-  const isPrivate = 'isPrivate' in data ? data.private : true;
+  const isPrivate = 'isPrivate' in data ? data.isPrivate : true;
 
   return {
     title,
