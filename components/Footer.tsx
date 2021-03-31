0d63c558d266107a0f65281ab9ea5f0fc0669726
Mateusz Titz <titzmateusz@gmail.com>
2021-04-01 00:04:38 +0200

    feat(Footer): add github project link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,22 @@ import Link from 'next/link';
 function Footer() {
   return (
     <footer className="footer">
-      <p>© Kicu 2021</p>
-      <Link href="/posts">
+      <div className="footer__footerLeft">
+        <span>© Kicu 2021</span>
+        <Link href="/posts">
           <a>← blog</a>
-      </Link>
+        </Link>
+      </div>
+      <div className="footer__footerRight">
+        <a href="https://github.com/Kicu/blog" className="footer__githubLink">
+          <img
+            src="/images/github-mark.png"
+            alt="github-mark"
+            className="footer__githubMark"
+          />
+          GitHub
+        </a>
+      </div>
     </footer>
   );
 }
