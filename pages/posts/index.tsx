aa7b342292c703ea1c58d43abad81e2ca53a9f63
Mateusz Titz <titzmateusz@gmail.com>
2021-04-12 18:33:07 +0200

    feat: add article subheading in articles list view and some style tweaks

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -3,7 +3,12 @@ import Link from 'next/link';
 
 import { getPostList } from '../../src/posts/getPostList';
 
-type SimplePostData = { title: string; slug: string; date: string };
+type SimplePostData = {
+  title: string;
+  subhead: string;
+  slug: string;
+  date: string;
+};
 
 function PostsIndexPage({ posts }: { posts: SimplePostData[] }) {
   return (
@@ -16,6 +21,7 @@ function PostsIndexPage({ posts }: { posts: SimplePostData[] }) {
               <Link href={postUrl}>
                 <a className="postsList__itemLink">{post.title}</a>
               </Link>
+              <p className="postsList__itemSubheading">{post.subhead}</p>
               <div>
                 🗓️<span className="postsList__itemDate">{post.date}</span>
               </div>
@@ -32,6 +38,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const formattedPosts = posts.map((postMetadata) => ({
     title: postMetadata.title,
+    subhead: postMetadata.subhead,
     slug: postMetadata.slug,
     date: postMetadata.createdDate,
   }));
--- src/posts/extractPostMetadata.ts
+++ src/posts/extractPostMetadata.ts
@@ -1,5 +1,6 @@
 function extractPostMetadata(data: any): PostMetadata {
   const title = 'title' in data ? data.title : '';
+  const subhead = 'subhead' in data ? data.subhead : 'Some generic subtitle here...';
   const id = 'id' in data ? data.id : '';
   const slug = 'slug' in data ? data.slug : '';
   const createdDate = 'createdDate' in data ? data.createdDate : '2020-02-20'; // Todo add sensible default
@@ -7,6 +8,7 @@ function extractPostMetadata(data: any): PostMetadata {
 
   return {
     title,
+    subhead,
     id,
     slug,
     createdDate,
