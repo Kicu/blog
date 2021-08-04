19a105a7264b492386229b1b0738a82625c7be69
Mateusz Titz <titzmateusz@gmail.com>
2021-08-04 23:19:34 +0200

    feat: add nice page title

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 function Header(): JSX.Element {
   return (
     <header className="header">
+      <Head>
+        <title>worlds in my head</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Link href="/posts">
         <h1>
           <a>worlds in my head</a>
