7eceeacad34ee685af0e89e7706f45baac510a66
Mateusz Titz <titzmateusz@gmail.com>
2022-07-16 13:42:01 +0200

    build: add ikar poem

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bfb59b2bb41d72968630";
+/******/ 	var hotCurrentHash = "5b73e996fd7c47859750";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
