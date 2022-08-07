214fa455e27e8ffd5e7a11068ed667508e0f78a9
Mateusz Titz <titzmateusz@gmail.com>
2022-08-07 15:33:38 +0200

    build: add new poem

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b73e996fd7c47859750";
+/******/ 	var hotCurrentHash = "c1e7ead4a62fffabaa8b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
